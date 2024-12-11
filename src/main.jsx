23057edbb00744f0d71e43b21e1c4d8a19c6912a
darkasfu <ashraful.islam.asfu@gmail.com>
2024-12-11 17:34:05 +0600

    Inital setup

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/router.jsx";
+import {NextUIProvider} from '@nextui-org/react'
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
+  </StrictMode>
+);
